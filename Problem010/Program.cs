3ad4a8664f5476a8dc663ac81c01ef58b31ca8e1
GuGuJelly <jellypuding@naver.com>
2024-08-11 19:10:42 +0900

    두 수의 합
    프로그래머스 24년 8월 11일

---
+++ Problem010/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem010
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 프로그래머스
+        /// 두 수의 합
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution(3,5));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = -1;
+            answer = num1 + num2;
+            return answer;
+        }
+    }
+}
