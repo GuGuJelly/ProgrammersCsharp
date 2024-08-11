8c21749b3e446bbb1b8ddf7e1f1b51a896e30d97
GuGuJelly <jellypuding@naver.com>
2024-08-11 17:13:43 +0900

    Update Program3.cs

--- Program3.cs
+++ Program3.cs
@@ -1,33 +1,39 @@
-﻿namespace Problem003
+namespace Probliem003
 {
     internal class Program
     {
         /// <summary>
-        /// 2024년 8월 5일
-        /// 사이트: 프로그래머스
-        /// 문제명: 두수의 곱
-        /// URL: https://school.programmers.co.kr/learn/courses/30/lessons/120804
+        /// 프로그래머스
+        /// 2024년 8월 11일
+        /// 숫자 비교하기
         /// </summary>
-        public class Solution
-        {
-            public int solution(int num1, int num2)
-            {
-                int answer = 0;
-                if ((num1 < 0 || num1 > 100) || (num2 < 0 || num2 > 100))
-                {
-                    Console.WriteLine("잘못된 범위의 숫자를 입력했습니다");
-                    answer = 0;
-                    return answer;
-                }
-                answer = num1 * num2;
-                return answer;
-            }
-        }
-
+        /// <param name="args"></param>ㅍ
         static void Main(string[] args)
         {
             Solution s = new Solution();
-            Console.WriteLine(s.solution(200, 2));
+            Console.WriteLine(s.solution(3, 100));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            if (num1 == num2)
+            {
+                answer = 1;
+            }
+            else if (num1 != num2)
+            {
+                answer = -1;
+            }
+            else if ((num1 < 0 || num1 > 10000) || (num2 < 0 || num2 > 10000))
+            {
+                Console.WriteLine("범위에 맞는 숫자를 입력해주세요");
+                answer = 0;
+            }
+            return answer;
         }
     }
 }
