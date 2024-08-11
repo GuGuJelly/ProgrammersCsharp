27e6d3e1f3c05c959fadbcd1d6aab987363032f8
GuGuJelly <jellypuding@naver.com>
2024-08-11 17:25:42 +0900

    몫 구하기
    프로그래머스 24년 8월 11일

---
+++ Problem005/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem005
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// 프로그래머스
+            /// 2024년 8월 11일
+            /// 몫 구하기
+            /// </summary>
+            /// <param name="args"></param>
+            Solution s = new Solution();
+            Console.WriteLine(s.solution(10,3));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            answer = num1 / num2;
+
+            return answer;
+        }
+    }
+}
