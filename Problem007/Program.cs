e19f7c862b09fe80fc7c6d804b3c4fd79dda76ec
GuGuJelly <jellypuding@naver.com>
2024-08-11 18:02:25 +0900

    두 수의 나눗셈
    프로그래머스 24년 8월 11일

---
+++ Problem007/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem007
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 프로그래머스
+        /// 2024년 8월 11일
+        /// 두 수의 나눗셈
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution(10,3));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            return (int)((num1 / (double)num2) * 1000);
+        }
+    }
+}
