c31c7d3bbfd52698fd881cb7e40cdcaf43509867
GuGuJelly <jellypuding@naver.com>
2024-08-18 01:00:47 +0900

    C#  배열뒤집기
    프로그래머스 24년 08월 18일

---
+++ Problem012/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num_list = { 1, 2, 3, 4, 5 };
+            Solution s = new Solution();
+            foreach (int num in s.solution(num_list)) 
+            { 
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            int[] answer = new int[num_list.Length];
+            int j = 0;
+            for (int i = num_list.Length - 1; i >= 0; i--)
+            {
+                answer[j] = num_list[i];
+                j++;
+            }
+            return answer;
+        }
+    }
+}
