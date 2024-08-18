3b380dd8edabeeb116d7728acac0f2bb8c0c2312
GuGuJelly <jellypuding@naver.com>
2024-08-18 22:14:37 +0900

    문자열 안에 문자열
    프로그래머스 24년 8월 18일

---
+++ Problem014/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string str1 = "ppprrrogrammers";
+            string str2 = "pppp";
+            Console.WriteLine($"{s.solution(str1, str2)}");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(string str1, string str2)
+        {
+            int answer = 0;
+            //str1 = "";
+            //str2 = "";
+            switch (str1.Contains(str2))
+            {
+                case true:
+                    answer = 1;
+                    break;
+                case false:
+                    answer = 2;
+                    break;
+            }
+
+            //str1.Contains(str2);
+            return answer;
+        }
+    }
+}
