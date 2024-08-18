6317053e23b625feb64a96434760bee6d743d3f0
GuGuJelly <jellypuding@naver.com>
2024-08-18 12:42:47 +0900

    배열 원소의 길이
    프로그래머스 2024년 8월 18일

--- Problem011/Program.cs
+++ Problem011/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// 프로그래머스: 배열 원소의 길이
+        /// 풀이날짜: 2024년 8월 18일
+        /// 작성자: 황규호
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] strlist = { "We", "are", "the", "world!" };
+            Solution s = new Solution();
+            s.solution(strlist);
+        }
+
+        public class Solution
+        {
+            public int[] solution(string[] strlist)
+            {
+                
+                int[] answer = new int[strlist.Length];
+                for (int i = 0 ; i< strlist.Length ; i++) 
+                {
+                    answer[i]=strlist[i].Length;
+                }
+
+                foreach (int a in answer) 
+                {
+                    Console.Write($"{a} ");
+                }
+
+                return answer;
+            }
         }
     }
 
