e7b8f2c53f4d39586fae86d1dbab598d1d789fa7
GuGuJelly <jellypuding@naver.com>
2024-11-11 00:30:39 +0900

    프로그래머스 문제016번
    풀이

---
+++ Problem016/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Problem016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine($"Jona의 문자열을 뒤집어서 {solution.solution("Jona")}로 만들었습니다");
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string my_string)
+        {
+            string answer = "";
+            string reverseStr = "";
+
+            for (int i = my_string.Length - 1; i >= 0; i--)
+            {
+                reverseStr += my_string[i];
+            }
+
+            answer = reverseStr;
+            return answer;
+        }
+    }
+}
