2777c720d6e3c78db45222571a04b60aa533e179
GuGuJelly <jellypuding@naver.com>
2024-11-20 21:02:00 +0900

    양꼬치
    머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다. 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.

---
+++ Problem017/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Problem017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            
+            Console.WriteLine(sol.solution(10,3));
+        }
+    }
+
+    public class Solution
+    {
+        int j = 0;
+        public int solution(int n, int k)
+        {
+            int answer = 0;
+            int ggoChi = 12000;
+            int drink = 2000;
+
+            answer = ggoChi * n + drink * k;
+            if (n >= 10)
+            {
+                for (int i = 10; i <= n; i+=10)
+                {
+                    j++;
+                }
+                answer -= drink * j;
+            }
+            return answer;
+        }
+    }
+}
