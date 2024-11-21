06588e4326e36e823e1f71333e2ebfb4f20919f0
GuGuJelly <jellypuding@naver.com>
2024-11-21 19:31:38 +0900

    세균증식
    프로그래머스 18번째 문제

---
+++ Problem018/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution(7,15));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n, int t)
+        {
+            int answer = 0;
+            if (t >=1 || t <= 15)
+            {
+                for (int i = 1; i <= t; i++)
+                {
+                    n = 2 * n;
+                    answer = n;
+                }
+            }else 
+                return answer;
+
+            return answer;
+        }
+    }
+}
