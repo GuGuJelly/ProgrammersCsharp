a08b388b184c852c608ead7980987d2ad8e51c74
GuGuJelly <jellypuding@naver.com>
2024-11-22 22:55:20 +0900

    배열에서 짝수와 홀수 세기

---
+++ Problem019/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int[] answer = [1, 2, 3, 4, 5];
+
+
+            foreach (int result in sol.solution(answer))
+            {
+                Console.Write($"[{result}]");
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] num_list)
+        {
+            int[] answer = new int[2];
+
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i <= num_list.Length - 1; i++)
+            {
+                if (num_list[i] % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else if (num_list[i] % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+
+            answer[0] = evenCount;
+            answer[1] = oddCount;
+
+            return answer;
+        }
+    }
+}
