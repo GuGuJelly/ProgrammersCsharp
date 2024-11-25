914c42b1e712d1482b623157e867ef28a85184aa
GuGuJelly <jellypuding@naver.com>
2024-11-25 22:15:33 +0900

    편지 문자열 길이

---
+++ Problem021/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            Console.WriteLine(sol.solution("Icecream"));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(string message)
+        {
+            int answer = 0;
+
+            answer = message.Length*2;
+
+            return answer;
+        }
+    }
+
+}
