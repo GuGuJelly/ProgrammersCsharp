002e44eae2b6b55b2cfc9f7c2b24ade9f1e7ad19
GuGuJelly <jellypuding@naver.com>
2024-11-25 22:27:12 +0900

    중복된 숫자 개수

---
+++ Problem022/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1,1,3,3,5,6 };
+            Solution sol = new Solution();
+            
+            Console.WriteLine(sol.solution(array,3));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] array, int n)
+        {
+            int answer = 0;
+
+            foreach (int a in array) 
+            {
+                if (a-n==0)
+                {
+                    answer += 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+
+}
