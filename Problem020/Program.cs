900be2e9eabd2321ab24cac643ac6d3b085588f1
GuGuJelly <jellypuding@naver.com>
2024-11-25 19:29:56 +0900

    점의 위치 구하기(사분면으로 표현)

---
+++ Problem020/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dot = { 1,2 };
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution(dot));
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] dot)
+        {
+            int answer = 0;
+
+            if (dot[0] > 0 && dot[1] > 0)
+            {
+                answer = 1;
+            }
+            else if (dot[0] < 0 && dot[1] > 0)
+            {
+                answer = 2;
+            }
+            else if (dot[0] < 0 && dot[1] < 0)
+            {
+                answer = 3;
+            }
+            else if (dot[0] > 0 && dot[1] < 0)
+            {
+                answer = 4;
+            }
+            return answer;
+        }
+    }
+
+}
