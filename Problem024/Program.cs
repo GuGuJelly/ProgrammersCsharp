d20708698a62766627a88b7036c7d82ed754a683
GuGuJelly <jellypuding@naver.com>
2024-11-30 19:34:25 +0900

    머쓱이보다 키 큰 사람

---
+++ Problem024/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int[] array = { 149, 180, 192, 170 };
+            Console.WriteLine($"{sol.solution(array,170)}");
+        }
+    }
+    public class Solution
+    {
+        public int solution(int[] array, int height)
+        {
+            int answer = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (height < array[i])
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+}
