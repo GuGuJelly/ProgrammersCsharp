6236bc5407939c443853302ae0eea356799d50d2
GuGuJelly <jellypuding@naver.com>
2024-11-30 20:22:34 +0900

    배열 두배 만들기

---
+++ Problem025/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int[] numbers = { 1,2,3,4,5 };
+            foreach (int number in sol.solution(numbers)) 
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+    public class Solution
+    {
+        public int[] solution(int[] numbers)
+        {
+            int[] answer = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer[i] = numbers[i]*2;
+            }
+
+            return answer;
+        }
+    }
+
+}
+
