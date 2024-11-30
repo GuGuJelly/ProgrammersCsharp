0c64ccdb784727ea412fe28dee4c3f84010e9be4
GuGuJelly <jellypuding@naver.com>
2024-11-30 23:41:57 +0900

    모음 제거

---
+++ Problem026/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem026
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Diagnostics.Tracing;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution("Nice to meet you"));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(string my_string)
+        {
+            string answer = "";
+            string[] arr = my_string.Split('a', 'e', 'i', 'u', 'o');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i].TrimEnd();
+            }
+
+            answer = string.Join("",arr);
+
+
+            return answer;
+        }
+    }
+}
+
