eb1216012e2a7d6e6cefce0a196a1ff3ffa2c187
GuGuJelly <jellypuding@naver.com>
2024-12-01 23:40:04 +0900

    짝수는 싫어요

---
+++ Problem027/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            foreach (int element in sol.solution(15))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+    public class Solution
+    {
+        public int[] solution(int n)
+        {
+            int size = (n + 1) / 2;
+            int[] answer = new int[size];
+
+            for (int i = 0; i < size; i++ )
+            {
+                answer[i] = 2*i + 1;
+            }
+
+            return answer;
+        }
+    }
+
+}
