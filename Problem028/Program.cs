d6095d626dfe974888c68166346aea0bf488e00a
GuGuJelly <jellypuding@naver.com>
2024-12-19 09:08:20 +0900

    [Feat] 임시
    임시

---
+++ Problem028/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem028
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            if (7/n < 1)
+            {
+                
+            }
+            
+
+            return answer;
+        }
+    }
+
+}
