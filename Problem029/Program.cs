a8be048ad336b3f3d13725779bd2e958af82c0c8
GuGuJelly <jellypuding@naver.com>
2025-03-24 01:47:19 +0900

    피자 나눠 먹기(1)

---
+++ Problem029/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem029
+{
+    using System;
+    using System.Runtime.InteropServices.Marshalling;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.Solution29(15));
+        }
+    }
+
+    class Solution
+    {
+        public int Solution29(int n) 
+        {
+            int answer = 0; // 피자의 수
+
+            if (n % 7  == 0)
+            {
+                answer = n / 7;
+            }
+            else if (n % 7 > 0)
+            {
+                answer = n / 7 + 1;
+            }
+                return answer;
+        }
+    }
+}
