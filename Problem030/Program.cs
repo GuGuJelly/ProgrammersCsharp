c4b40250cd19836e83f6d93d0048d3a1df87d401
GuGuJelly <jellypuding@naver.com>
2025-03-24 18:20:08 +0900

    피나 나눠 먹기(3)

---
+++ Problem030/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem030
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(4,12));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int slice, int n)
+        {
+            int answer = 0;
+            
+            if (n % slice == 0)
+            {
+                answer = n / slice;
+            } 
+            else 
+                answer = n / slice + 1;
+            
+
+            return answer;
+        }
+    }
+}
