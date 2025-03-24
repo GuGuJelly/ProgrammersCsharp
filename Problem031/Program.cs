3a8dbed978a67fb1e8d3f624f424fb1fc2d8a222
GuGuJelly <jellypuding@naver.com>
2025-03-24 22:32:58 +0900

    특정 문자 제거하기

---
+++ Problem031/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution("apple", "a"));
+        }
+    }
+
+    class Solution
+    {
+        public string solution(string my_string, string letter)
+        {
+            string answer = "";
+            answer = my_string.Replace(letter,"");
+
+
+            return answer;
+        }
+    }
+}
