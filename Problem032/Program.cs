605e75215a4fe87d53eff2b684101d4927ca1a6f
GuGuJelly <jellypuding@naver.com>
2025-03-25 19:47:11 +0900

    최댓값 만들기(1)

---
+++ Problem032/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Problem032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution([1,2,3,4,5]));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] numbers)
+        {
+            int answer = 0;
+           
+            Array.Sort(numbers);
+            answer = numbers[numbers.Length - 1] * numbers[numbers.Length - 2];
+
+            return answer;
+        }
+    }
+}
