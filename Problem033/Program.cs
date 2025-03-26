72ac3ea54772323ce215bb644b1fc6be8f80a25e
GuGuJelly <jellypuding@naver.com>
2025-03-26 17:05:22 +0900

    삼각형의 완성조건(1)

---
+++ Problem033/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem033
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] sides)
+        {
+            int answer = 0;
+
+            Array.Sort(sides);
+            if (sides[2] < sides[0] + sides[1])
+            {
+                answer = 1;
+            }else
+                answer = 2;
+
+                return answer;
+        }
+    }
+}
