fb658c578ab2ee6f344290c90718873a600d2aa3
GuGuJelly <jellypuding@naver.com>
2025-03-28 22:40:02 +0900

    자릿수 더하기

---
+++ Problem036/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Problem036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(12345));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            while (n > 0) 
+            {
+                answer += n % 10;
+                n = n / 10;
+            }
+
+            return answer;
+        }
+    }
+}
