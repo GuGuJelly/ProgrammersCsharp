dd343928e1c7c166e61d2ef7fc4fc6a8295bb9be
GuGuJelly <jellypuding@naver.com>
2025-03-29 16:17:32 +0900

    아이스 아메리카노

---
+++ Problem037/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(string.Join(", ", solution.solution(1100)));
+        }
+    }
+
+    class Solution
+    {
+        public int[] solution(int money)
+        {
+            int[] answer = new int[2];
+
+            answer[0] = money / 5500;
+            answer[1] = money - (answer[0] * 5500);
+
+            return answer;
+        }
+    }
+
+}
