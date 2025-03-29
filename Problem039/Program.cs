9508c6e3d9b1ee61d84ba032ffcc35e311fd5976
GuGuJelly <jellypuding@naver.com>
2025-03-29 19:02:32 +0900

    제곱수 판별하기

---
+++ Problem039/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem039
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(300));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            int number = 0;
+
+            for (int i = 1;i<=n;i++)
+            {
+                if (i*i==n)
+                {
+                    answer = 1;
+                    number++;
+                    break;
+                } else if (number <= 0)
+                {
+                    answer = 2;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
