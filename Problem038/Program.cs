981ae39e2efdf74ef00c0e86a15916d7facdb1b9
GuGuJelly <jellypuding@naver.com>
2025-03-29 18:41:56 +0900

    순서쌍의 개수

---
+++ Problem038/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(100));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            int number = 1;
+            while (number <= n) 
+            {
+                if (n % number <= 0)
+                {
+                    answer++;
+                }
+                number++;
+            }
+            return answer;
+        }
+    }
+}
