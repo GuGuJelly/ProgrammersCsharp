7c90c760e7169931a5e60c27ff9f48e41a54c830
GuGuJelly <jellypuding@naver.com>
2025-03-30 17:55:33 +0900

    옷가게 할인 받기(when절 사용, 버전 문제로 통과안됨)

---
+++ Problem040/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Problem040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(150000));
+        }
+    }
+
+    /// <summary>
+    /// 버전 문제로 when절이 안먹히는 풀이
+    /// </summary>
+    class Solution
+    {
+        public float solution(float price)
+        {
+            float answer = 0;
+            switch (price)
+            {
+                case float n when n >= 100000 && n < 300000:
+                    answer = price - price * 0.05f;
+                    break;
+
+                case float n when n >= 300000 && n < 500000:
+                    answer = price - price * 0.1f;
+                    break;
+
+                case float n when n >= 500000:
+                    answer = price - price * 0.2f;
+                    break;
+                    
+                default:
+                    answer = price;
+                    break;
+            }
+
+            return answer;
+        }
+    }
+}
