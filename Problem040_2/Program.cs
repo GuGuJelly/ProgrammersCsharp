4a11d4c3d91dc30cfe9ed6432ede8b9c22df9c88
GuGuJelly <jellypuding@naver.com>
2025-03-30 18:13:10 +0900

    옷가게 할인 받기(형변환 풀이)

---
+++ Problem040_2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem040_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(150000));
+        }
+    }
+ 
+    class Solution
+    {
+        public int solution(int price)
+        {
+            int answer = 0;
+
+            float answerFloat = (float)answer;
+            if (price >= 100000 && price < 300000)
+            {
+                answerFloat = price - price * (0.05f);
+            }else if (price >= 300000 && price < 500000)
+            {
+                answerFloat = price - price * (0.1f);
+            }else if (price >= 500000)
+            {
+                answerFloat = price - price * (0.2f);
+            }else
+                answerFloat = price;
+
+            answer = (int)answerFloat;
+                return answer;
+        }
+    }
+}
